bd508e5e254c05fb983b250ecb48cd97d7f0d586
ArifulSikder <mdislamnoyon3@gmail.com>
2023-08-21 22:35:42 +0600

    Env dynamically show done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -64,43 +64,29 @@ class SettingController extends Controller
             'name' => $request->name
         ];
 
-        $mailer = $data['mailer'];
-        $host = $data['host'];
-        $port = $data['port'];
-        $username = $data['username'];
-        $password = $data['password'];
-        $encription = $data['encription'];
-        $address = $data['address'];
-        $name = $data['name'];
-        
         $envFile = app()->environmentFilePath();
         $content = file_get_contents($envFile);
-
-
-        $str1 = str_replace("MAIL_MAILER=".env('MAIL_MAILER'), "MAIL_MAILER=$mailer\n", $content);
-        file_put_contents($envFile, $str1);
-
-        $str2 = str_replace("MAIL_HOST=".env('MAIL_HOST'), "MAIL_HOST=$host\n", $content);
-        file_put_contents($envFile, $str2);
-
-        $str3 = str_replace("MAIL_PORT=".env('MAIL_PORT'), "MAIL_PORT=$port\n", $content);
-        file_put_contents($envFile, $str3);
-
-        $str4 = str_replace("MAIL_USERNAME=".env('MAIL_USERNAME'), "MAIL_USERNAME=$username\n", $content);
-        file_put_contents($envFile, $str4);
-
-        $str5 = str_replace("MAIL_PASSWORD=".env('MAIL_PASSWORD'), "MAIL_PASSWORD=$password\n", $content);
-        file_put_contents($envFile, $str5);
-
-        $str6 = str_replace("MAIL_ENCRYPTION=".env('MAIL_ENCRYPTION'), "MAIL_ENCRYPTION=$encription\n", $content);
-        file_put_contents($envFile, $str6);
-
-        $str7 = str_replace("MAIL_FROM_ADDRESS=".env('MAIL_FROM_ADDRESS'), "MAIL_FROM_ADDRESS=$address\n", $content);
-        file_put_contents($envFile, $str7);
-
-        $str8 = str_replace("MAIL_FROM_NAME=".env('MAIL_FROM_NAME'), "MAIL_FROM_NAME=$name\n", $content);
-        file_put_contents($envFile, $str8);
-
+        
+        $replacements = [
+            'MAIL_MAILER' => $data['mailer'],
+            'MAIL_HOST' => $data['host'],
+            'MAIL_PORT' => $data['port'],
+            'MAIL_USERNAME' => $data['username'],
+            'MAIL_PASSWORD' => $data['password'],
+            'MAIL_ENCRYPTION' => $data['encription'],
+            'MAIL_FROM_ADDRESS' => $data['address'],
+            'MAIL_FROM_NAME' => $data['name'],
+        ];
+        
+        foreach ($replacements as $key => $newValue) {
+            $content = preg_replace(
+                "/^{$key}=.*/m",
+                "{$key}={$newValue}",
+                $content
+            );
+        }
+        
+        file_put_contents($envFile, $content);
 
         
         // $this->editMailer($data);

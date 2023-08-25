0568267acbe7458a0aa087ea3dd3d81e25a3017e
ArifulSikder <mdislamnoyon3@gmail.com>
2023-08-25 23:48:13 +0600

    Seo done

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -5,9 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {!! SEOMeta::generate() !!}
+    {!! OpenGraph::generate() !!}
+    {!! Twitter::generate() !!}
+    {!! JsonLd::generate() !!}
+    
     <!--====== Title ======-->
     <title>@yield('title')</title>
     <!--====== Favicon Icon ======-->
